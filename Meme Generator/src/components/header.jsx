8009a794636967480666428dce969e05b321247f
Sammar Hayat <sammarhayat1122@gmail.com>
2023-09-19 12:22:07 +0500

    project setup and create header

---
+++ Meme Generator/src/components/header.jsx
@@ -0,0 +1,10 @@
+// import React from "react"
+
+export default function Header(){
+    return(
+        <header className="nav">
+       <img className="nav-logo" src="./images/troll-face.png"></img>
+       <h2 className="nav-title">Meme Generator</h2>
+       <h3 className="nav-text">React Course - Project 3</h3>
+    </header>
+    )}
